5b816f623480c7adb9eb16a43c3a7cfad9f8acf8
yahya09206 <yahyahussein91@gmail.com>
2026-07-03 22:27:35 -0700

    completed navigation test case 3

---
+++ src/test/java/com/yahya/Navigation/NavigationCaseThree.java
@@ -0,0 +1,34 @@
+package com.yahya.Navigation;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NavigationCaseThree {
+
+    public static void main(String[] args) {
+
+        /*
+         * Test Case 3: Navigate Back
+         * Objective: Verify navigate().back() works correctly
+         * Navigate to https://www.tesla.com
+         * Navigate to https://www.google.com
+         * Call navigate().back()
+         * Verify you're back on Tesla URL (contains "tesla")
+         */
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.saucedemo.com");
+
+        driver.get("https://www.google.com");
+
+        driver.navigate().back();
+
+        Assertions.assertTrue(driver.getTitle().contains("Swag"));
+
+        driver.quit();
+    }
+}
