c938c2d5a2053e533f1c2facce9802cbd7647174
yahya09206 <yahyahussein91@gmail.com>
2026-07-06 23:44:00 -0700

    completed test case 4

--- src/test/java/com/yahya/Navigation/NavigationCaseFour.java
+++ src/test/java/com/yahya/Navigation/NavigationCaseFour.java
@@ -1,6 +1,7 @@
 package com.yahya.Navigation;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -10,6 +11,27 @@ public class NavigationCaseFour {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
 
-        driver.get("");
+        driver.get("https://www.saucedemo.com");
+
+        /**
+         * Test Case 4: Navigate Forward
+         *
+         * Objective: Verify navigate().forward() works correctly
+         *
+         *
+         * Navigate to https://www.tesla.com
+         * Navigate to https://www.google.com
+         * Call navigate().back()
+         * Call navigate().forward()
+         * Verify you're on Google URL (contains "google")
+         */
+
+        driver.get("https://www.google.com");
+        driver.navigate().back();
+        driver.navigate().forward();
+
+        Assertions.assertTrue(driver.getTitle().contains("Google"));
+
+        driver.quit();
     }
 }
